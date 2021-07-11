b2d2324b7c266bba5deb34630a61cf32eb2ca228
DesiSanou <desire.sanoupro@gmail.com>
2021-07-11 20:32:35 +0200

    Package Version 0.1 commit

---
+++ setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup, find_packages
+setup(name='file_manipulator', version='0.1',
+      packages=find_packages(), author='Sanou Désiré, See GitHub: https://github.com/DesiSanou',
+author_email='desire.sanoupr@gmail.com')
